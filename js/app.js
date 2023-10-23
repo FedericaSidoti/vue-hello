e442a79d85f5a3792fea5d186dc4cc150b8b1f1b
FedericaSidoti <federicasidoti21@gmail.com>
2023-10-23 17:04:41 +0200

    ADD- changing color button and title

--- js/app.js
+++ js/app.js
@@ -4,8 +4,17 @@ createApp ({
     data() {
         return {
             message: 'Questo è il mio primo applicativo Vue',
-            image: 'https://picsum.photos/600/800',
-            colour: 'green'
+            image: 'https://picsum.photos/800/600',
+            coloursArray : ['green', 'red', 'violet', 'orange', 'brown'],
+            colour: '',
+            index: 0,
+        }
+    },
+    methods: {
+        changeColour: function() {
+            this.colour = this.coloursArray[this.index]
+            console.log(this.colour)
+            this.index++
         }
     }
 }).mount('#app')
